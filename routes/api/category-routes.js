1ad2154ea3df3245508f38d11cabf02c9db8ec42
renusushmak <sushmakr533@gmail.com>
2021-08-07 12:36:02 -0700

    finished models finished category and product routes

---
+++ routes/api/category-routes.js
@@ -0,0 +1,61 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  Category.findAll( {
+    include: [Product]
+  }).then(data => {
+    res.json(data)
+  }).catch( err => {
+    res.json(err)
+  } )
+});
+
+router.get('/:id', (req, res) => {
+  Category.findOne( {
+    where: {
+      id:req.params.id
+    },
+    include: [Product]
+  }).then(data => {
+    res.json(data)
+  }).catch( err => {
+    res.json(err)
+  } )
+});
+
+router.post('/', (req, res) => {
+  Category.create(req.body).then(data => {
+    res.json(data)
+  }).catch( err => {
+    res.json(err)
+  } )
+});
+
+router.put('/:id', (req, res) => {
+  Category.update(req.body, {
+    where: {
+       id:req.params.id
+      }
+    }).then(data => {
+        res.json(data)
+      }).catch( err => {
+        res.json(err)
+      } )
+});
+
+router.delete('/:id', (req, res) => {
+  Category.delete({
+    where: {
+      id:req.params.id
+    }
+  }).then(data => {
+    res.json(data)
+  }).catch( err => {
+    res.json(err)
+  } )
+});
+
+module.exports = router;
